4a2bfa76d44bc62a8dd6c9a28b993e104b10c1d6
vekeveke <sultanfabian6@gmail.com>
2022-04-08 17:48:54 +0700

    Airplane

---
+++ Airplane/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public GameObject plane;
+    private Vector3 offset = new Vector3(28, 3, 4);
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
